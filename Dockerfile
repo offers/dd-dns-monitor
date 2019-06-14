e78b512516eb9ea6547f8f91839aeeb685e90837
Chris Kite <chris@chriskite.com>
2019-06-14 13:54:02 -0500

    updates for k8s

--- Dockerfile
+++ Dockerfile
@@ -1,34 +1,9 @@
-FROM phusion/baseimage:0.9.18
+FROM golang:1.12
 
-ENV GOPATH=/go
-ENV PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
-ENV GO15VENDOREXPERIMENT=1
-ENV MONITOR_ROOT=/go/src/github.com/offers/dd-dns-monitor
+WORKDIR /go/src/app
+COPY . .
 
-RUN apt-get update \
-    && apt-get install -y wget git \
-    && apt-get clean \
-    && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+RUN go get -d -v ./...
+RUN go install -v ./...
 
-# install and setup go
-RUN wget https://storage.googleapis.com/golang/go1.6.linux-amd64.tar.gz \
-    && tar -C /usr/local -xzf go1.6.linux-amd64.tar.gz \
-    && rm go1.6.linux-amd64.tar.gz \
-    && mkdir /go
-
-# install glide
-RUN mkdir /glide \
-    && cd /glide \
-    && wget https://github.com/Masterminds/glide/releases/download/0.10.1/glide-0.10.1-linux-amd64.tar.gz \
-    && tar -xzf glide-0.10.1-linux-amd64.tar.gz \
-    && mv linux-amd64/glide /usr/local/bin/ \
-    && rm -rf /glide
-
-# build dd-dns-monitor
-ADD . /go/src/github.com/offers/dd-dns-monitor
-RUN cd $MONITOR_ROOT \
-    && glide install \
-    && go install
-
-RUN mkdir /etc/service/dd-dns-monitor \
-    && cp $MONITOR_ROOT/run /etc/service/dd-dns-monitor/run
+CMD ["app"]
--- main.go
+++ main.go
@@ -3,20 +3,24 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"os"
 	"strings"
 	"time"
 
-	"github.com/DataDog/datadog-go/statsd"
 	"github.com/codegangsta/cli"
 	"github.com/miekg/dns"
 )
 
-func dnsCheck(name, ip, server string) (error, time.Duration) {
-	c := dns.Client{}
-	m := dns.Msg{}
+func dnsCheck(name, ip, server string, timeout time.Duration) (error, time.Duration) {
+	m := new(dns.Msg)
 	m.SetQuestion(name+".", dns.TypeA)
-	r, t, err := c.Exchange(&m, server+":53")
+
+	c := new(dns.Client)
+	c.Dialer = &net.Dialer{
+		Timeout: timeout,
+	}
+	r, t, err := c.Exchange(m, server+":53")
 
 	if err != nil {
 		return err, t
@@ -37,7 +41,7 @@ func dnsCheck(name, ip, server string) (error, time.Duration) {
 	return nil, t
 }
 
-func monitor(dd *statsd.Client, name, ip string, servers []string, interval time.Duration) {
+func monitor(name, ip string, servers []string, interval time.Duration, timeout time.Duration) {
 	log.Printf("Monitoring...")
 
 	ticker := time.NewTicker(interval)
@@ -45,13 +49,12 @@ func monitor(dd *statsd.Client, name, ip string, servers []string, interval time
 		select {
 		case <-ticker.C:
 			for _, server := range servers {
-				tag := []string{server}
-				err, t := dnsCheck(name, ip, server)
+				err, t := dnsCheck(name, ip, server, timeout)
+				ms := int32(t/time.Millisecond)
 				if err != nil {
-					log.Printf("DNS Check Error: %v", err.Error())
-					dd.Count("error", 1, tag, 1)
+					log.Printf("DNS Check Error (%dms): %v", ms, err.Error())
 				} else {
-					dd.TimeInMilliseconds("time", float64(t/time.Millisecond), tag, 1)
+					log.Printf("DNS Check Success (%dms)", ms)
 				}
 			}
 		}
@@ -61,11 +64,11 @@ func monitor(dd *statsd.Client, name, ip string, servers []string, interval time
 func main() {
 	app := cli.NewApp()
 	app.Name = "dd-dns-monitor"
-	app.Usage = "log DNS server failures to DataDog"
+	app.Usage = "log DNS server failures"
 	app.Author = "Chris Kite"
 
-	var name, ip, servers, datadogHost string
-	var interval time.Duration
+	var name, ip, servers string
+	var interval, timeout time.Duration
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
@@ -86,36 +89,30 @@ func main() {
 			EnvVar:      "DNS_SERVERS",
 			Destination: &servers,
 		},
-		cli.StringFlag{
-			Name:        "datadog, d",
-			Usage:       "Datadog collector host",
-			EnvVar:      "DATADOG_HOST",
-			Destination: &datadogHost,
+		cli.DurationFlag{
+			Name:        "timeout, t",
+			Usage:       "dns query timeout",
+			Value:       5 * time.Second,
+			EnvVar:      "DNS_INTERVAL",
+			Destination: &timeout,
 		},
 		cli.DurationFlag{
-			Name:        "interval, t",
+			Name:        "interval, l",
 			Usage:       "interval in seconds to check at",
-			Value:       15,
+			Value:       500 * time.Millisecond,
 			EnvVar:      "DNS_INTERVAL",
 			Destination: &interval,
 		},
 	}
 
 	app.Action = func(c *cli.Context) {
-		if "" == name || "" == ip || "" == servers || "" == datadogHost {
+		if "" == name || "" == ip || "" == servers {
 			cli.ShowAppHelp(c)
 			return
 		}
 
-		namespace := "dd_dns_monitor"
-		dd, err := statsd.New(datadogHost + ":8125")
-		if err != nil {
-			log.Fatal(err)
-		}
-		dd.Namespace = namespace + "."
-
 		servers := strings.Split(c.String("servers"), ",")
-		monitor(dd, name, ip, servers, interval*time.Second)
+		monitor(name, ip, servers, interval, timeout)
 	}
 
 	app.Run(os.Args)
